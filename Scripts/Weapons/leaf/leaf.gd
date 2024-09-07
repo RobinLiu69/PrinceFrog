e93f17727831c8392b887e10155b1ccc00b3cc23
z9487xd <135353453+z9487xd@users.noreply.github.com>
2024-09-07 22:32:28 +0800

    Add files via upload

---
+++ Scripts/Weapons/leaf/leaf.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+
+
+@onready var timer: Timer = $Timer 
+var from: CharacterBody2D = null
+var basic_damage: int
+var speed: float
+var existing_time: float = -1
+var maker: Marker2D = null
+var targets: Array[Node] = []
+var revolution_radius: float
+var rotation_speed: float 
+
+func _ready():
+	pass
+
+func _process(delta):
+	move_and_slide()
+
+func hit(body: CharacterBody2D) -> void:
+	maker.hit_counter()
+
+
+func _on_leaf_hit_body_entered(body: Node2D) -> void:
+	if body.has_method("handle_hit") and body != from:
+		var total_damage = basic_damage
+		body.handle_hit(from, total_damage)
+		hit(body)
